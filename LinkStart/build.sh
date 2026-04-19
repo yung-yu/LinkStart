ca0b390b1cce6341a7d146b3c213ec867c2227c5
AndyLi <yungyu405728@gmail.com>
2026-04-19 09:54:52 +0800

    chore: bump version to 1.1

--- LinkStart/build.sh
+++ LinkStart/build.sh
@@ -41,9 +41,9 @@ cat << 'PLIST' > "$APP_DIR/Contents/Info.plist"
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.0</string>
+    <string>1.1</string>
     <key>CFBundleVersion</key>
-    <string>1</string>
+    <string>2</string>
     <key>LSMinimumSystemVersion</key>
     <string>12.0</string>
     <key>NSHighResolutionCapable</key>
